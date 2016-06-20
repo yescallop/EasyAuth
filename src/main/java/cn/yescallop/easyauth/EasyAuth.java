6bafd2ac76755ace9ec2fa310b40dc578b3e591b
yescallop <s@yescallop.cn>
2016-06-20 13:46:12 +0800

    Push all codes

---
+++ src/main/java/cn/yescallop/easyauth/EasyAuth.java
@@ -0,0 +1,159 @@
+package cn.yescallop.easyauth;
+
+import cn.nukkit.Player;
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.Config;
+import cn.nukkit.utils.Utils;
+import cn.yescallop.easyauth.lang.BaseLang;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.UUID;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class EasyAuth extends PluginBase {
+
+    private BaseLang lang;
+    private byte[] salt;
+    private File playersFolder;
+    private ArrayList<Player> authedPlayers = new ArrayList<>();
+
+    @Override
+    public void onEnable() {
+        this.getDataFolder().mkdirs();
+        File saltFile = new File(this.getDataFolder(), "salt.txt");
+        try {
+            if (!saltFile.exists()) {
+                saltFile.createNewFile();
+                Utils.writeFile(saltFile, getRandomSalt());
+            }
+            String salt = Utils.readFile(saltFile);
+            this.salt = new BASE64Decoder().decodeBuffer(salt);
+        } catch (IOException e) {
+            this.salt = new byte[0];
+        }
+        playersFolder = new File(this.getDataFolder(), "players");
+        playersFolder.mkdirs();
+        lang = new BaseLang(this.getServer().getLanguage().getLang());
+        this.getServer().getPluginManager().registerEvents(new EventListener(this), this);
+        for (Player player : this.getServer().getOnlinePlayers().values()) {
+            if (isPlayerLastUUID(player)) {
+                authenticatePlayer(player);
+            }
+        }
+        this.getLogger().info(lang.translateString("easyauth.loaded"));
+    }
+    
+    private String getRandomSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return new BASE64Encoder().encode(salt);
+    }
+    
+    public BaseLang getLanguage() {
+        return lang;
+    }
+    
+    public void authenticatePlayer(Player player) {
+        authedPlayers.add(player);
+        setPlayerLastUUID(player);
+    }
+    
+    public void deauthenticatePlayer(Player player) {
+        authedPlayers.remove(player);
+    }
+    
+    public boolean isPlayerAuthenticated(Player player) {
+        return authedPlayers.contains(player);
+    }
+    
+    private byte[] getEncryptedPassword(String password) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+            digest.update(salt);
+            digest.update(password.getBytes("UTF-8"));
+        } catch (Exception e) {
+            return new byte[0];
+        }
+        return digest.digest();
+    }
+    
+    private String getEncryptedPasswordString(String password) {
+        return new BASE64Encoder().encode(getEncryptedPassword(password));
+    }
+    
+    public boolean checkPlayerPassword(Player player, String password) {
+        Config config = getPlayerConfig(player);
+        String realPasswordStr = config.getString("password");
+        if (realPasswordStr == null) return false;
+        byte[] realPassword;
+        try {
+            realPassword = new BASE64Decoder().decodeBuffer(realPasswordStr);
+        } catch (IOException e) {
+            return false;
+        }
+        byte [] encryptedPassword = getEncryptedPassword(password);
+        return Arrays.equals(realPassword, encryptedPassword);
+    }
+    
+    public void setPlayerPassword(Player player, String password) {
+        Config config = getPlayerConfig(player);
+        String encryptedPassword = getEncryptedPasswordString(password);
+        config.set("password", encryptedPassword);
+        config.save();
+    }
+    
+    public void registerPlayer(Player player, String password) {
+        setPlayerPassword(player, password);
+        authenticatePlayer(player);
+    }
+    
+    public void unregisterPlayer(Player player) {
+        deauthenticatePlayer(player);
+        getPlayerConfigFile(player).delete();
+    }
+    
+    public boolean isPlayerRegistered(Player player) {
+        return getPlayerConfigFile(player).exists();
+    }
+    
+    public void setPlayerLastUUID(Player player) {
+        Config config = getPlayerConfig(player);
+        config.set("lastUUID", player.getUniqueId().toString());
+        config.save();
+    }
+    
+    public UUID getPlayerLastUUID(Player player) {
+        if (isPlayerRegistered(player)) {
+            Config config = getPlayerConfig(player);
+            String uuid = config.getString("lastUUID");
+            return UUID.fromString(uuid);
+        } else {
+            return null;
+        }
+    }
+    
+    public boolean isPlayerLastUUID(Player player) {
+        UUID uuid = getPlayerLastUUID(player);
+        if (uuid == null) return false;
+        return player.getUniqueId().equals(uuid);
+    }
+    
+    public File getPlayerConfigFile(Player player) {
+        return new File(playersFolder, player.getName().toLowerCase() + ".yml");
+    }
+    
+    public Config getPlayerConfig(Player player) {
+        return new Config(getPlayerConfigFile(player), Config.YAML);
+    }
+}
---
+++ src/main/java/cn/yescallop/easyauth/EventListener.java
@@ -0,0 +1,155 @@
+package cn.yescallop.easyauth;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.block.BlockBreakEvent;
+import cn.nukkit.event.block.BlockPlaceEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.event.inventory.InventoryPickupItemEvent;
+import cn.nukkit.event.player.PlayerChatEvent;
+import cn.nukkit.event.player.PlayerCommandPreprocessEvent;
+import cn.nukkit.event.player.PlayerDropItemEvent;
+import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.event.player.PlayerItemConsumeEvent;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.event.player.PlayerMoveEvent;
+import cn.nukkit.event.player.PlayerPreLoginEvent;
+import cn.nukkit.event.player.PlayerQuitEvent;
+import cn.nukkit.inventory.InventoryHolder;
+import cn.yescallop.easyauth.lang.BaseLang;
+
+import java.util.HashMap;
+
+public class EventListener implements Listener {
+
+    EasyAuth plugin;
+    BaseLang lang;
+    HashMap<Player, String> confirmWaiting = new HashMap<>();
+
+    public EventListener(EasyAuth plugin) {
+        this.plugin = plugin;
+        lang = plugin.getLanguage();
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (plugin.isPlayerLastUUID(player)) {
+            plugin.authenticatePlayer(player);
+            player.sendMessage(lang.translateString("login.auto"));
+        } else {
+            player.sendMessage(plugin.isPlayerRegistered(player) ? lang.translateString("login.input") : lang.translateString("register.input"));
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerChat(PlayerChatEvent event) {
+        Player player = event.getPlayer();
+        String message = event.getMessage();
+        if (!plugin.isPlayerAuthenticated(player)) {
+            event.setCancelled();
+            if (!plugin.isPlayerRegistered(player)) {
+                if (!confirmWaiting.containsKey(player)) {
+                    player.sendMessage(lang.translateString("register.confirm", message));
+                    confirmWaiting.put(player, message);
+                } else {
+                    if (!message.equals("back")) {
+                        if (message.equals(confirmWaiting.get(player))) {
+                            plugin.registerPlayer(player, message);
+                            player.sendMessage(lang.translateString("register.success"));
+                            confirmWaiting.remove(player);
+                        } else {
+                            player.sendMessage(lang.translateString("register.notmatching"));
+                        }
+                    } else {
+                        player.sendMessage(lang.translateString("register.reinput"));
+                        confirmWaiting.remove(player);
+                    }
+                }
+            } else {
+                if (plugin.checkPlayerPassword(player, message)) {
+                    plugin.authenticatePlayer(player);
+                    player.sendMessage(lang.translateString("login.success"));
+                } else {
+                    player.kick(lang.translateString("login.fail"));
+                    Server.getInstance().getNetwork().blockAddress(player.getAddress(), 30);
+                }
+            }
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onInventoryPickupItem(InventoryPickupItemEvent event) {
+        InventoryHolder holder = event.getInventory().getHolder();
+        if (holder instanceof Player && !plugin.isPlayerAuthenticated((Player) holder)) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer()) && event.getMessage().startsWith("/")) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (!plugin.isPlayerAuthenticated(event.getPlayer())) {
+            event.setCancelled();
+        }
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        plugin.deauthenticatePlayer(player);
+        confirmWaiting.remove(player);
+    }
+    
+    @EventHandler(ignoreCancelled = true)
+    public void onEntityDamage(EntityDamageEvent event) {
+        Entity entity = event.getEntity();
+        if (entity instanceof Player && !plugin.isPlayerAuthenticated((Player) entity)) {
+            event.setCancelled();
+        }
+    }
+}
